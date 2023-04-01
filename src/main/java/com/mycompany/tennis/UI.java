bf8da80d3fed486b7cd858f96c7eb50851697277
PeterFlandin <peterflandin@gmail.com>
2023-04-02 00:10:52 +0200

    Controller

---
+++ src/main/java/com/mycompany/tennis/UI.java
@@ -0,0 +1,21 @@
+package com.mycompany.tennis;
+
+import com.mycompany.tennis.controller.JoueurController;
+
+public class UI {
+
+    public static void main(String... args){
+
+      //  JoueurController joueurController = new JoueurController();
+      //  joueurController.afficheDetailJoueur();
+
+
+JoueurController joueurController = new JoueurController();
+joueurController.creerJoueur();
+
+    }
+
+
+
+
+}
